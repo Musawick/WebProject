47be58943b2d5cccb61fdc23e2497e71dee2fa00
Musawick Wahid <91912420+Musawick@users.noreply.github.com>
2023-07-19 03:25:55 +0600

    Update app.py

--- MainWeb/app.py
+++ MainWeb/app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 # Configs app
 app = Flask(__name__)
 
-# I assume the user will be redirected to login page once they enter web
+# I assume the user will be redirected to login page once they enter web. Plz inform me if you decide on something else
 @app.route("/")
 def index():
     return render_template("index.html")
